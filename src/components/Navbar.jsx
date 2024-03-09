8e4ef7eb072cdbbebf82e6db6b7349386674f30a
Duouk2000 <duouk2000@gmail.com>
2024-03-09 21:20:01 +0000

    updated rendering and basic formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 
 function CustomNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" bg="success" variant="dark">
       <Container>
-        <Navbar.Brand as={Link} to="/">Portfolio</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/home">Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />        
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/home">Home</Nav.Link>
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,7 +3,29 @@ import React from 'react';
 function Project() {
   return (
     <div>      
-      <h2>Projects Page</h2>
+      <h2>Literary-Lounge</h2>
+        <h5>GitHub: https://github.com/Duouk2000/Literary-Lounge</h5>
+        <h5>Deployed Website: https://duouk2000.github.io/Literary-Lounge</h5>
+        
+      <h2>Weather-Dashboard</h2>
+        <h5>GitHub: https://github.com/Duouk2000/Weather-Dashboard</h5>
+        <h5>Deployed Website: https://duouk2000.github.io/Weather-Dashboard</h5>
+      
+      <h2>Workday-Calendar</h2>
+        <h5>GitHub: https://github.com/Duouk2000/Workday-Calendar</h5>
+        <h5>Deployed Website: https://duouk2000.github.io/Workday-Calendar/</h5>
+
+      <h2>JS-Code-Quiz</h2>
+        <h5>GitHub: https://github.com/Duouk2000/JS-Code-Quiz</h5>
+        <h5>Deployed Website: https://duouk2000.github.io/JS-Code-Quiz</h5>
+
+      <h2>Secure-Password-Generator</h2>
+        <h5>GitHub: https://github.com/Duouk2000/Secure-Password-Generator</h5>
+        <h5>Deployed Website: https://duouk2000.github.io/Secure-Password-Generator</h5>
+
+      <h2>Team-Profile-Generator</h2>
+        <h5>GitHub: https://github.com/Duouk2000/Team-Profile-Generator</h5>
+        <h5>Deployed Website: https://github.com/Duouk2000/Team-Profile-Generator</h5>
     </div>
   );
 }
