63b0c252eb2196e7c8710a72e3518ea9199910b4
Duouk2000 <duouk2000@gmail.com>
2024-03-09 20:47:14 +0000

    updated basic filrd

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+function CustomNavbar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />        
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/home">Home</Nav.Link>
+          <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact Me</Nav.Link>
+        </Nav>        
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CustomNavbar;
