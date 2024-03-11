fbaf42a54cc9b47e1ab482ae4d44a8b904391ac6
Duouk2000 <duouk2000@gmail.com>
2024-03-11 22:22:25 +0000

    added cv and updated code

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,10 +14,10 @@ function Contact() {
   return (
     <div style={containerStyle}>
       <h3>Contact Information</h3>
-      <h5 style={linkStyle}><a href="https://github.com/duouk2000" target="_blank" rel="noopener noreferrer">GitHub Profile </a></h5>
+      <h5 style={linkStyle}><a href="https://github.com/duouk2000" target="_blank" rel="noopener noreferrer">GitHub Profile</a></h5>
       <h5 style={linkStyle}><a href="https://www.linkedin.com/in/andrew-sinkinson-68b26612a/" target="_blank" rel="noopener noreferrer">LinkedIn Page</a></h5>
-      <h5 style={linkStyle}><a href="mailto:duouk2000@gmail.com">Email Me</a></h5>
-      <h5 style={linkStyle}><a href="link-to-your-resume">Resume</a></h5>
+      <h5 style={linkStyle}><a href="mailto:duouk2000@gmail.com" aria-label="Email Me">Email Me</a></h5>
+      <h5 style={linkStyle}><a href="./Andrew-Sinkinson-CV.pdf" target="_blank" rel="noopener noreferrer">Resume</a></h5>
     </div>
   );
 }
